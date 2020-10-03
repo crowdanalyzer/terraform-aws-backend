bf84852075544997ccbd3132069610f037e2aa5e
Bahaa Galal <bahaa.g.ali@gmail.com>
2020-10-03 13:10:01 +0200

    add a remote backend example usage

---
+++ examples/s3-remote-backend/backend.tf
@@ -0,0 +1,15 @@
+# ------------------------------------------------------------------------------------------------------------------
+# USE AN AMAZON S3 BUCKET TO REMOTELY STORE TERRAFORM STATE
+# USE A DYNAMODB TABLE FOR STATE UPDATE LOCKS
+# ------------------------------------------------------------------------------------------------------------------
+
+terraform {
+  backend "s3" {
+    bucket = "aria-stark.remote-backend.terraform"
+    key = "examples/s3-remote-backend/terraform.tfstate"
+    region = "us-east-1"
+
+    dynamodb_table = "aria-stark-remote-backend-terraform-dynamodb-locks-table"
+    encrypt = true
+  }
+}
---
+++ examples/s3-remote-backend/variables.tf
@@ -0,0 +1,30 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ENVIRONMENT VARIABLES
+# Define these secrets as environment variables
+# ---------------------------------------------------------------------------------------------------------------------
+
+# AWS_ACCESS_KEY_ID
+# AWS_SECRET_ACCESS_KEY
+
+# ------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ------------------------------------------------------------------------------------------------------------------
+
+variable "bucket_name" {
+  description = "The name of the s3 bucket that is used for this module."
+  type        = string
+  default     = "sansa-stark.remote-backends.terraform.example"
+}
+
+variable "region" {
+  description = "The region where the s3 bucket exists."
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "tags" {
+  description = "The tags that should be associated with the resources created by this module."
+  type        = map(string)
+  default     = {}
+}
